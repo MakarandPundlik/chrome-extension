a104207ca7ccdf1cce4efa19096d412282e1126c
MakarandPundlik <mmp2308@gmail.com>
2021-06-09 21:36:10 +0530

    accessing dad jokes

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,47 +1,36 @@
-
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const Timer = () => {
+  const [[mins, secs], setTime] = useState([20, 0]);
+  const [meme, setMeme] = useState(false);
+ 
+  const tick = () => {
+    if (mins === 0 && secs === 0) setMeme(true);
+    else if (secs === 0) setTime([mins - 1, 59]);
+    else setTime([mins, secs - 1]);
+  };
+
+  
+  useEffect(() => {
    
-    
-    const [[hrs, mins, secs], setTime] = React.useState([0, 20, 0]);
-    
+    const timerId = setInterval(() => tick(), 1000);
+    return () => clearInterval(timerId);
+  });
 
-    const tick = () => {
-   
-        if (hrs === 0 && mins === 0 && secs === 0) 
-            reset()
-        else if (mins === 0 && secs === 0) {
-            setTime([hrs - 1, 59, 59]);
-        } else if (secs === 0) {
-            setTime([hrs, mins - 1, 59]);
-        } else {
-            setTime([hrs, mins, secs - 1]);
-        }
-    };
-
-
-    const reset = () => setTime([parseInt(0), parseInt(20), parseInt(0)]);
-
-    
-    React.useEffect(() => {
-        const timerId = setInterval(() => tick(), 1000);
-        return () => clearInterval(timerId);
-    });
-
-    
-    return (
-       <div className="timerContainer">
-       <div className="header">
-            Look at object 20 feet away for 20 seconds
-        </div>
-        <div className="timer">
-            <p>{`${mins
-            .toString()
-            .padStart(2, '0')}:${secs.toString().padStart(2, '0')}`}</p> 
-        </div>
-       </div>
-    );
-}
+  return (
+    <div className="timerContainer">
+      <div className="header">Look at object 20 feet away for 20 seconds</div>
+      <div className="timer">
+        <p>{`${mins.toString().padStart(2, "0")}:${secs
+          .toString()
+          .padStart(2, "0")}`}</p>
+      </div>
+      {
+          meme && 
+          <h1>Here is a meme</h1>
+      }
+    </div>
+  );
+};
 
 export default Timer;
