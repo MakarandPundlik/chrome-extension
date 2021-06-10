0249863bc3fb75911d21211bfdcab53f77b486c5
MakarandPundlik <mmp2308@gmail.com>
2021-06-10 11:24:02 +0530

    added meme component

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 
-const Timer = () => {
-  const [[mins, secs], setTime] = useState([20, 0]);
-  const [meme, setMeme] = useState(false);
- 
+const Timer = (props) => {
+  const [[mins, secs], setTime] = useState([20, 20]);
+  const[timeout,setTimeout] = useState(false);
   const tick = () => {
-    if (mins === 0 && secs === 0) setMeme(true);
+    if (mins === 0 && secs === 0) {
+      setTime([20,20]);
+      props.history.push("/meme")
+    }
     else if (secs === 0) setTime([mins - 1, 59]);
     else setTime([mins, secs - 1]);
   };
@@ -25,10 +27,6 @@ const Timer = () => {
           .toString()
           .padStart(2, "0")}`}</p>
       </div>
-      {
-          meme && 
-          <h1>Here is a meme</h1>
-      }
     </div>
   );
 };
